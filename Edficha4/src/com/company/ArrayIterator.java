2b3adca8e00a92f77c6206d76b1b0bc955ffec49
Abílio <abilio_mg_castro@hotmail.com>
2018-11-20 16:13:29 +0000

    penis

--- Edficha4/src/com/company/ArrayIterator.java
+++ Edficha4/src/com/company/ArrayIterator.java
@@ -1,4 +1,22 @@
 package com.company;
 
-public class ArrayIterator {
+import java.util.Iterator;
+
+public class ArrayIterator<T> implements Iterator<T> {
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
 }
